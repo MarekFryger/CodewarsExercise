b153f287e00d35cec9661b8b0d371427b5900424
Marek Fryger <marekfryger@o2.pl>
2022-02-11 17:14:21 +0100

    Task VowelCount done with test

--- src/main/java/codewars7kyu/VowelCount/VowelCount.java
+++ src/main/java/codewars7kyu/VowelCount/VowelCount.java
@@ -2,6 +2,7 @@ package codewars7kyu.VowelCount;
 
 public class VowelCount {
     public static int getCount(String str) {
+
         int vowelsCount = 0;
         char[] toCharArray = str.toCharArray();
         for(char c:toCharArray){
