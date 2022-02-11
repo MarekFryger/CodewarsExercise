df6164607ea3218510f2084a200f2adb945aafe9
Marek Fryger <marekfryger@o2.pl>
2022-02-11 17:07:25 +0100

    Task VowelCount done with test

---
+++ src/test/java/codewars7kyu/VowelCount/VowelCountTest.java
@@ -0,0 +1,13 @@
+package codewars7kyu.VowelCount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VowelCountTest {
+
+    @Test
+    public void testCase1() {
+        assertEquals( 5, VowelCount.getCount("abracadabra"));
+    }
+}
